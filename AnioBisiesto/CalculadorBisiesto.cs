c36896498a267eefe11ddde8a88b085a4e4a492f
oamelunge <oamelunge@innsa.com>
2014-09-22 14:16:09 -0400

    primer commit

---
+++ AnioBisiesto/CalculadorBisiesto.cs
@@ -0,0 +1,28 @@
+﻿namespace AnioBisiesto
+{
+    public class CalculadorBisiesto
+    {
+        private static int anio;
+
+        public CalculadorBisiesto(int _anio)
+        {
+            anio = _anio;
+        }
+
+        public bool es_bisiesto()
+        {
+            return EsAnioBisiestoTipico() && !EsAnioComunNoTipico();
+        }
+
+        private static bool EsAnioBisiestoTipico()
+        {
+            CalculadorBisiesto.anio = anio;
+            return CalculadorBisiesto.anio%4 == 0;
+        }
+
+        private static bool EsAnioComunNoTipico()
+        {
+            return anio%100==0 && anio%400 != 0;
+        }
+    }
+}
---
+++ AnioBisiesto/TestAnioBisiesto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnioBisiesto
+{
+    [TestClass] 
+    public class TestAnioBisiesto
+    {
+        
+
+        [TestMethod]
+        public void TestAnioBisiestoComun()
+        {
+            Assert.IsTrue(llamarACalculadorBisiestos(1996));
+        }
+
+        [TestMethod]
+        public void TestAnioCoum()
+        {
+            Assert.IsFalse(llamarACalculadorBisiestos(2001));
+        }
+
+        [TestMethod]
+        public void TestAnioComunNoTipicoDivisibleX4yNoX100()
+        {            
+            Assert.IsFalse(llamarACalculadorBisiestos(1900));
+        }
+
+        [TestMethod]
+        public void TestAnioBisiestoNoTipicoDivisiblePor100()
+        {            
+            Assert.IsTrue(llamarACalculadorBisiestos(2000));
+        }
+
+        private bool  llamarACalculadorBisiestos(int anio)
+        {
+            CalculadorBisiesto _calculadorBisiesto = new CalculadorBisiesto(anio);
+            return _calculadorBisiesto.es_bisiesto();
+        }
+    }
+}
